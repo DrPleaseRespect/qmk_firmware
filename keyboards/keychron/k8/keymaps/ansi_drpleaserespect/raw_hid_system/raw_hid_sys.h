70ca77054f341ecd6e9761675fd30d1e000a7082
DrPleaseRespect <61642760+DrPleaseRespect@users.noreply.github.com>
2022-07-25 20:43:26 +0800

    Add Copyright and stuff

--- keyboards/keychron/k8/keymaps/ansi_drpleaserespect/raw_hid_system/raw_hid_sys.h
+++ keyboards/keychron/k8/keymaps/ansi_drpleaserespect/raw_hid_system/raw_hid_sys.h
@@ -1,4 +1,24 @@
+/* Copyright 2020 Kasper
+ * Copyright 2022 DrPleaseRespect <DrPleaseRespect@outlook.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
+
+#define VolumeSTART 17
+#define RAW_EPSIZE 32
 
 enum raw_hid_functions {
   HID_SET_COLOR = 1,
@@ -11,13 +31,10 @@ enum raw_hid_functions {
 };
 
 
-#define VolumeSTART 17
-#define RAW_EPSIZE 32
-
-// CODE BREAKS IF RAW_EPSIZE IS TOO SMALL 
+// CODE BREAKS IF RAW_EPSIZE IS TOO SMALL
 // (might increase in the future)
-#if RAW_EPSIZE < 3
-#   error RAW_EPSIZE TOO SMALL! (>2)
+#if RAW_EPSIZE < 32
+#   error RAW_EPSIZE TOO SMALL! (>=32)
 #endif
 uint8_t SystemVolume;
 void VolumeLevelIndicator(bool pressed);
